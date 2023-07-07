4e825070242ae00d5f97afabedb1ef551ed071c6
ccpic <13426257846@163.com>
2023-07-07 10:24:14 +0800

    Added __repr__ for DfAnalyzer

--- dataframe.py
+++ dataframe.py
@@ -261,6 +261,16 @@ class DfAnalyzer:
         self.sorter = sorter
         self.save_path = save_path
 
+    def __repr__(self) -> str:
+        """函数描述
+
+        Returns:
+            str: 返回一个函数描述字符串
+        """
+        df = a.data
+        columns = ", ".join(df.columns.to_list())
+        return f"DfAnalyzer({df.shape[0]} rows, {df.shape[1]} columns)\nColumns: {columns}"
+
     def unit_change(
         self, unit_str: Optional[Literal["十亿", "亿", "百万", "万", "千"]]
     ) -> int:
@@ -565,51 +575,52 @@ class DfAnalyzer:
 
 if __name__ == "__main__":
     df = pd.read_excel("data.xlsx", engine="openpyxl")
-    a = DfAnalyzer(data=df, name="test", date_column="Date")
+    a = DfAnalyzer(data=df, name="test", date_column="Date", period_interval=3)
     a = a.transform(
         period="MAT",
         cols_grouper=["分子+年份+降幅", "CORPORATION", "PACKAGE", "数值类型"],
         cols_amount="数值",
     )
-    pivoted = (
-        a.get_pivot(
-            index=["谈判年份", "药品名称"],
-            columns=a.date_column,
-            query_str="数值类型=='金额'",
-            values="数值",
-        )
-        .div(100000000)
-        .reset_index()
-        .set_index("药品名称")
-    )
-    print(pivoted)
+    print(a)
+    # pivoted = (
+    #     a.get_pivot(
+    #         index=["谈判年份", "药品名称"],
+    #         columns=a.date_column,
+    #         query_str="数值类型=='金额'",
+    #         values="数值",
+    #     )
+    #     .div(100000000)
+    #     .reset_index()
+    #     .set_index("药品名称")
+    # )
+    # print(pivoted)
 
-    f = plt.figure(
-        FigureClass=GridFigure,
-        width=11,
-        height=10,
-        ncols=1,
-        fontsize=11,
-        style={
-            "title": "Test",
-            "label_outer": False,
-        },
-    )
+    # f = plt.figure(
+    #     FigureClass=GridFigure,
+    #     width=11,
+    #     height=10,
+    #     ncols=1,
+    #     fontsize=11,
+    #     style={
+    #         "title": "Test",
+    #         "label_outer": False,
+    #     },
+    # )
 
-    f.plot_bubble(
-        data=pivoted,
-        ax_index=0,
-        style={},
-        x="2022-12",
-        y="2021-12",
-        z="2022-12",
-        hue="谈判年份",
-        label_limit=100,
-        label_formatter="{index}\n({x}, {y})",
-        x_fmt="{:,.1f}",
-        y_fmt="{:,.1f}",
-        show_hist=True,
-        show_legend=False,
-    )
+    # f.plot_bubble(
+    #     data=pivoted,
+    #     ax_index=0,
+    #     style={},
+    #     x="2022-12",
+    #     y="2021-12",
+    #     z="2022-12",
+    #     hue="谈判年份",
+    #     label_limit=100,
+    #     label_formatter="{index}\n({x}, {y})",
+    #     x_fmt="{:,.1f}",
+    #     y_fmt="{:,.1f}",
+    #     show_hist=True,
+    #     show_legend=False,
+    # )
 
-    f.save()
+    # f.save()
